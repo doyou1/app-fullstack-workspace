5e17027b87ad6a27f9dc48a6af06545dafdbc734
doyou1 <tla149@daum.net>
2023-01-15 11:48:38 +0900

    image serve, upload backend feature complete

--- backend/backend/src/main/kotlin/com/accountbook/backend/ImageController.kt
+++ backend/backend/src/main/kotlin/com/accountbook/backend/ImageController.kt
@@ -1,56 +1,57 @@
 package com.accountbook.backend
 
-import jakarta.servlet.annotation.MultipartConfig
 import jakarta.servlet.http.HttpServletRequest
-import org.apache.commons.logging.Log
-import org.springframework.web.bind.annotation.ModelAttribute
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RequestPart
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.multipart.MultipartFile
+import org.springframework.core.io.ClassPathResource
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.util.StreamUtils
+import org.springframework.web.bind.annotation.*
 import java.io.File
 import java.io.FileOutputStream
+
 
 @RestController
 @RequestMapping("/api")
 class ImageController {
 
-    @RequestMapping("/get", method = [RequestMethod.POST])
-    fun get(
-        @RequestBody names: List<String>
-    ): HashMap<String, ByteArray> {
+    @RequestMapping("/get/{fileName}", method = [RequestMethod.GET])
+    fun getImage(
+        @PathVariable("fileName")
+        fileName: String
+    ): ResponseEntity<ByteArray> {
+        val file = ClassPathResource("images/$fileName")
+        val bytes = StreamUtils.copyToByteArray(file.inputStream)
 
-        println(names)
-
-        return hashMapOf()
+        return ResponseEntity
+            .ok()
+            .contentType(MediaType.IMAGE_JPEG)
+            .body(bytes)
     }
 
     @RequestMapping("/upload", method = [RequestMethod.POST])
     fun upload(
         request: HttpServletRequest
     ): Int {
-        val dirPath ="${getAbsolutePath()}/backend/src/main/resources/images"
+        val dirPath = "${getAbsolutePath()}/backend/src/main/resources/images"
         val dir = File(dirPath)
-        if(!dir.exists()) {
+        if (!dir.exists()) {
             dir.mkdir()
         }
 
+        var count = 0
         for (item in request.parts) {
             if (item.contentType == "image/jpg; charset=utf-8") {
                 val file = File("$dirPath/${item.submittedFileName}")
                 val fout = FileOutputStream(file)
                 item.inputStream.transferTo(fout)
                 fout.close()
+                count++
             }
         }
-
-        return -1
+        return count
     }
 
-    private fun getAbsolutePath() : String {
+    private fun getAbsolutePath(): String {
         return File("").absolutePath
     }
 }
