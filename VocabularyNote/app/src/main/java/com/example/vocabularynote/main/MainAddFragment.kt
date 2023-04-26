8047b1d8a113c835c3aa0296451da6d180909e35
doyou1 <tla149@daum.net>
2023-04-26 23:34:48 +0900

    hide keyboard feature

--- VocabularyNote/app/src/main/java/com/example/vocabularynote/main/MainAddFragment.kt
+++ VocabularyNote/app/src/main/java/com/example/vocabularynote/main/MainAddFragment.kt
@@ -1,15 +1,18 @@
 package com.example.vocabularynote.main
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.vocabularynote.R
 import com.example.vocabularynote.databinding.FragmentMainAddBinding
+import com.google.android.material.textfield.TextInputEditText
 
 class MainAddFragment : Fragment() {
 
@@ -28,7 +31,26 @@ class MainAddFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // This callback will only be called when MyFragment is at least Started.
+
+        aboutKeyboard()
+        setBackPressEvent()
+    }
+
+    private fun aboutKeyboard() {
+        binding.layoutWrap.setOnClickListener {
+            if (it !is TextInputEditText) {
+                hideKeyboard()
+            }
+        }
+    }
+
+    private fun hideKeyboard() {
+        val im =
+            requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        im.hideSoftInputFromWindow(requireActivity().currentFocus?.windowToken, 0)
+        requireActivity().currentFocus?.clearFocus()
+    }
+    private fun setBackPressEvent() {
         val callback: OnBackPressedCallback =
             object : OnBackPressedCallback(true /* enabled by default */) {
                 override fun handleOnBackPressed() {
